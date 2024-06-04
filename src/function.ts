c4e1f41f30a2c65d579f6e1ea2d5b5cc079c217c
olawolejethro <kunlekzi2@gmail.com>
2024-06-04 03:29:03 +0100

    input_output

--- src/function.ts
+++ src/function.ts
@@ -1,19 +1,18 @@
 /** @format */
 
-function add(n1: number, n2: number) {
+function Add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printresult(num: number): void {
   console.log("result :" + num);
 }
 
+printresult(Add(10, 12));
 function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
   const result = n1 + n1;
   cb(result);
 }
-
-printResult(add(10, 12));
 
 // when you use void that means you are not returning anything in the function
 
@@ -21,5 +20,5 @@ let combineValues: (a: number, b: number) => number;
 //  here we are telling javascript that the function assign to combine value can only take two parameter which are numbers and also return a number
 // function type allow us describe which type of function specifically we  want to user somewher
 
-combineValues = add;
+combineValues = Add;
 console.log(combineValues(5, 3));
