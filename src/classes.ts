a6243bdb8e3615bedf7bf178aaeced9387d2d92c
olawolejethro <kunlekzi2@gmail.com>
2024-06-20 12:44:54 +0100

    classses

--- src/classes.ts
+++ src/classes.ts
@@ -1,16 +1,18 @@
 /** @format */
 
-class Department {
+abstract class Department {
   // private id : string,
   // public name: string;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     this.name = name;
   }
-  describe(this: Department) {
-    console.log(`Department (${this.id}):${this.name}`);
-  }
+  // describe(this: Department) {
+  //   console.log(`Department (${this.id}):${this.name}`);
+  // }
+
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -27,9 +29,13 @@ class ITDepartment extends Department {
     super(id, "IT");
     this.admins = admins;
   }
+  describe() {
+    console.log(`ccounting Department - ID ${this.id}`);
+  }
 }
 
 class AccountingDepartment extends Department {
+  static fiscalYEar = 2020;
   private lastReport: string;
 
   get mostRecentReport() {
@@ -46,16 +52,23 @@ class AccountingDepartment extends Department {
     this.addReport(v);
   }
 
-  constructor(id: string, private reports: string[]) {
+  constructor(id: string, protected reports: string[]) {
     super(id, "IT");
     this.lastReport = reports[0];
   }
 
+  describe() {
+    console.log(`ccounting Department - ID ${this.id}`);
+  }
   addEmployee(name: string): void {
     if (name === "MAX") {
       return;
     }
     this.employees.push(name);
+  }
+
+  static createEmployee(name: string) {
+    return { name };
   }
   addReport(text: string) {
     this.reports.push(text);
@@ -65,23 +78,27 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
+
+const employee1 = AccountingDepartment.createEmployee("MAk");
+console.log(employee1, AccountingDepartment.fiscalYEar);
 // kkk
-const accounting = new Department("Id", "Accounting");
+// const accounting = new Department("Id", "Accounting");
 const results = new AccountingDepartment("AA", []);
+
+console.log(results.describe());
 
 results.mostRecentReport = "jet";
 console.log(results.mostRecentReport);
 results.addReport("somthing went wrong");
 results.printReports();
 console.log("here");
-const cheif = accounting.describe();
-
-console.log(cheif);
+// const cheif = accounting.describe();
 
 // accounting.addEmployee("Max");
 // accounting.addEmployee("MANND");
-accounting.addEmployee("jetthro");
-accounting.printEmployeeInfo();
+// accounting.addEmployee("jetthro");
+// accounting.printEmployeeInfo();
 
+// console.log(accounting.describe());
 // const accountingCopy = { name: "Dummy", describe: accounting.describe };
 // accountingCopy.describe();
