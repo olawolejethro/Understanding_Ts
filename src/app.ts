75f2a73187adcdf88352e776e5dc3e92492cdc1b
olawolejethro <kunlekzi2@gmail.com>
2024-06-25 15:52:53 +0100

    intercept

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,23 @@
 /** @format */
-//  interfaces describle the structure of an obeject,it can also be use as a type check for obj
-// just like a model
-// using interface as function
 
-interface Addfn {
-  (a: number, b: number): number;
-}
-
-let adds: Addfn;
-adds = (n1: number, n2: number) => n1 + n2;
-
-interface Named {
-  readonly name: string;
-}
-
-interface greetable extends Named {
+type admin = {
   name: string;
-  age: number;
+  privileges: string[];
+};
 
-  greet(phrase: string): void;
-}
-
-class Person implements greetable {
+type employee = {
   name: string;
-  age = 29;
-  // let user1: person;
+  startDate: Date;
+};
 
-  constructor(n: string) {
-    this.name = n;
-  }
+type elevatedEmployee = admin & employee;
+const e1: elevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
 
-  greet(phrase: string) {
-    console.log(`${phrase}  ${this.name}`);
-  }
-}
+type Combinable = string | number;
+type Numeric = number | boolean;
 
-let user1: greetable;
-user1 = new Person("mac");
-console.log(user1);
-
-user1.greet(`hi!!! MY NAME IS `);
-
-// user1.greet("Hi there - I am");
-
-// WHY INTERFACE
+type universal = combinable;
